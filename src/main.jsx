378c7d491b21942ed4958c889451cfea588f70b1
rakanugraha18 <rakanugraha2@gmail.com>
2023-11-16 19:41:20 +0700

    Fix Page Poco with Atomic Design

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,6 @@ import About from "./pages/About.jsx";
 import Agreement from "./pages/Agreement.jsx";
 import Leadership from "./pages/Leadership.jsx";
 import Privacy from "./pages/Privacy.jsx";
-import PocoSection from "./components/molecule/Smartphone/PocoSection.jsx";
 import PocoList from "./components/molecule/Smartphone/PocoList.jsx";
 
 {
@@ -35,7 +34,6 @@ const router = createBrowserRouter([
       { path: "/Agreement", element: <Agreement /> },
       { path: "/Leadership", element: <Leadership /> },
       { path: "/Privacy", element: <Privacy /> },
-      { path: "/PocoSection", element: <PocoSection /> },
       { path: "/PocoList", element: <PocoList /> },
     ],
   },
